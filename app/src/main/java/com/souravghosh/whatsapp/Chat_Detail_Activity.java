a393fabea7617ab33f3178bc9f03d2152eaa35d6
Sourav Ghosh <infosourav04@gmail.com>
2021-01-03 20:22:13 +0530

    Initial Commit

--- app/src/main/java/com/souravghosh/whatsapp/Chat_Detail_Activity.java
+++ app/src/main/java/com/souravghosh/whatsapp/Chat_Detail_Activity.java
@@ -1,5 +1,6 @@
 package com.souravghosh.whatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
@@ -8,14 +9,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.souravghosh.whatsapp.Adapter.ChatAdapter;
 import com.souravghosh.whatsapp.Modeles.MassagesModel;
 import com.souravghosh.whatsapp.databinding.ActivityChatDetailBinding;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Chat_Detail_Activity extends AppCompatActivity {
 
@@ -57,6 +63,47 @@ public class Chat_Detail_Activity extends AppCompatActivity {
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         binding.chatrecyclerview.setLayoutManager(layoutManager);
-        
+
+        final String senderroom = senderId + reciveId;
+        final String reciverroom = reciveId + senderId;
+
+        database.getReference().child("Chats").child(senderroom).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                massagesModels.clear();
+                for (DataSnapshot snapshot1 : snapshot.getChildren()){
+                    MassagesModel model = snapshot1.getValue(MassagesModel.class);
+                    massagesModels.add(model);
+                }
+                chatAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        binding.send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = binding.etmsg.getText().toString();
+                final  MassagesModel model = new MassagesModel(senderId, message);
+                model.setTimetamp(new Date().getTime());
+                binding.etmsg.setText("");
+
+                database.getReference().child("Chats").child(senderroom).push().setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        database.getReference().child("Chats").child(reciverroom).push().setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
 }
